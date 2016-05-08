1c172236620636090e22708e6a1ae7fcaaac345e
badele <bruno@adele.im>
2016-05-08 17:49:15 +0200

    Add dimension and exporting features

--- freecad/shelf/self_generator.py
+++ freecad/shelf/self_generator.py
@@ -1,7 +1,7 @@
 #! python
 # -*- coding: utf-8 -*-
 
-import Part, math
+import Part, Draft, math
 import FreeCAD, FreeCADGui
 
 # Create document
@@ -92,6 +92,19 @@ def show_inventory():
         print ""
         print ""
 
+def DrawDim(p1,p2,p3,name,extlines=250):
+    # Dimension
+    dim = Draft.makeDimension(p1,p2,p3)
+    dim.Label = name
+    dim.ViewObject.DisplayMode ="3D"
+    dim.ViewObject.ExtLines = extlines
+    dim.ViewObject.Decimals = 0
+    dim.ViewObject.ArrowType = "Arrow"
+    dim.ViewObject.ArrowSize = 0
+    dim.ViewObject.FontSize = 0.2
+    dim.ViewObject.TextSpacing = 200
+    dim.ViewObject.LineColor = (0.75, 0.75, 0.75)
+
 
 # Create customized shelf
 def generate_self(nbcols=2, nbrows=8, colwidth=250, rowhight=300, depth=400, woodwidth=20, name='etagere',
@@ -130,21 +143,96 @@ def generate_self(nbcols=2, nbrows=8, colwidth=250, rowhight=300, depth=400, woo
     return doc.getObject(name)
 
 
+# Global Parameters
+DEPTH=400
+WOODWIDTH=20
+NBROWS=9
+ROWHIGHT=300
+DIMLEVEL1=2*ROWHIGHT
+DIMLEVEL2=ROWHIGHT
+
 # Left Part
-shelfA = generate_self(nbcols=2, nbrows=9, colwidth=250, name='meuble_gauche')
+shelfA = generate_self(nbcols=2, nbrows=NBROWS, rowhight=ROWHIGHT, colwidth=250, name='meuble_gauche', depth=DEPTH, woodwidth=WOODWIDTH)
 shelfA.ViewObject.ShapeColor = (0.80, 0.80, 0.40)
 
 # Central part
-shelfB = generate_self(nbcols=1, nbrows=9, colwidth=500, ignorerows=[5, 6, 7], name='meuble_central')
+shelfB = generate_self(nbcols=1, nbrows=NBROWS, rowhight=ROWHIGHT, colwidth=500, ignorerows=[5, 6, 7], name='meuble_central', depth=DEPTH, woodwidth=WOODWIDTH)
 shelfB.Placement.Base = FreeCAD.Vector(560, 0, 0)
 shelfB.ViewObject.ShapeColor = (0.27, 0.80, 0.80)
 
 # Right part
-shelfC = generate_self(nbcols=1, nbrows=9, colwidth=500, ignorerows=[5, 7], name='meuble_droit')
+shelfC = generate_self(nbcols=1, nbrows=NBROWS, rowhight=ROWHIGHT, colwidth=500, ignorerows=[5, 7], name='meuble_droit', depth=DEPTH, woodwidth=WOODWIDTH)
 shelfC.Placement.Base = FreeCAD.Vector(1100, 0, 0)
 shelfC.ViewObject.ShapeColor = (0.80, 0.53, 0.80)
+
+#Init Dimensions
+FreeCADGui.activateWorkbench("DraftWorkbench")
+totalhight = NBROWS*(ROWHIGHT+WOODWIDTH)+(2*WOODWIDTH)
+totalwidth = 1640
+col1=0
+col2=3*WOODWIDTH+500
+col3=col2+2*WOODWIDTH+500
+
+# Horizontal plane
+p1 = FreeCAD.Vector(col1,0,0)
+p2 = FreeCAD.Vector(col2,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL1,0)
+DrawDim(p1,p2,p3,"Col 1",extlines=DIMLEVEL1-50)
+#
+p1 = FreeCAD.Vector(col1+WOODWIDTH,0,0)
+p2 = FreeCAD.Vector(col1+WOODWIDTH+250,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL2,0)
+DrawDim(p1,p2,p3,"Col 1.1",extlines=DIMLEVEL2-50)
+#
+p1 = FreeCAD.Vector(col1+WOODWIDTH+250+WOODWIDTH,0,0)
+p2 = FreeCAD.Vector(col1+WOODWIDTH+250+WOODWIDTH+250,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL2,0)
+DrawDim(p1,p2,p3,"Col 1.2",extlines=DIMLEVEL2-50)
+#
+p1 = FreeCAD.Vector(col2,0,0)
+p2 = FreeCAD.Vector(col3,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL1,0)
+DrawDim(p1,p2,p3,"Col 2",extlines=DIMLEVEL1-50)
+#
+p1 = FreeCAD.Vector(col2+WOODWIDTH,0,0)
+p2 = FreeCAD.Vector(col3-WOODWIDTH,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL2,0)
+DrawDim(p1,p2,p3,"Col 2.1",extlines=DIMLEVEL2-50)
+#
+p1 = FreeCAD.Vector(col3,0,0)
+p2 = FreeCAD.Vector(col3+2*WOODWIDTH+500,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL1,0)
+DrawDim(p1,p2,p3,"Col 3",extlines=DIMLEVEL1-50)
+#
+p1 = FreeCAD.Vector(col3+WOODWIDTH,0,0)
+p2 = FreeCAD.Vector(col3+WOODWIDTH+500,0,0)
+p3 = FreeCAD.Vector(0,-DIMLEVEL2,0)
+DrawDim(p1,p2,p3,"Col 3.1",extlines=DIMLEVEL2-50)
+#
+FreeCAD.DraftWorkingPlane.alignToPointAndAxis(FreeCAD.Vector(0,0,0), FreeCAD.Vector(0,-1,0), -400.0)
+p1 = FreeCAD.Vector(totalwidth,DEPTH,0)
+p2 = FreeCAD.Vector(totalwidth,DEPTH,totalhight)
+p3 = FreeCAD.Vector(totalwidth+DIMLEVEL2,DEPTH,0)
+DrawDim(p1,p2,p3,"Total width", extlines=DIMLEVEL2-50)
+
+# Vertical plane
+FreeCAD.DraftWorkingPlane.alignToPointAndAxis(FreeCAD.Vector(0,0,0), FreeCAD.Vector(0,0,1), 0.0)
+p1 = FreeCAD.Vector(0,0,totalhight)
+p2 = FreeCAD.Vector(totalwidth,0,totalhight)
+p3 = FreeCAD.Vector(0,DEPTH+DIMLEVEL2,totalhight)
+DrawDim(p1,p2,p3,"Total hight",extlines=DIMLEVEL2-50)
+#
+p1 = FreeCAD.Vector(0,0,WOODWIDTH)
+p2 = FreeCAD.Vector(0,0,WOODWIDTH+ROWHIGHT)
+p3 = FreeCAD.Vector(-DIMLEVEL2,0,0)
+DrawDim(p1,p2,p3,"Raw hight",extlines=DIMLEVEL2-50)
 
 # Show inventory
 show_inventory()
 
 FreeCAD.ActiveDocument.recompute()
+
+# Save picture
+FreeCADGui.activeDocument().activeView().viewAxonometric()
+FreeCADGui.SendMsgToActiveView("ViewFit")
+FreeCADGui.activeDocument().activeView().saveImage('sample.png', 4096, 3072,'Current')
